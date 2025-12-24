fd9f71b85757662917d55266b1ffe4466bd82d08
Marek Sobol <mswizual@gmail.com>
2025-12-24 18:25:09 +0100

    Aktualizacja - usuniecie linkedin

---
+++ assets/js/main.js
@@ -0,0 +1,305 @@
+/*
+	Multiverse by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ '481px',   '736px'  ],
+			xsmall:  [ null,      '480px'  ]
+		});
+
+	// Hack: Enable IE workarounds.
+		if (browser.name == 'ie')
+			$body.addClass('ie');
+
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('touch');
+
+	// Transitions supported?
+		if (browser.canUse('transition')) {
+
+			// Play initial animations on page load.
+				$window.on('load', function() {
+					window.setTimeout(function() {
+						$body.removeClass('is-preload');
+					}, 100);
+				});
+
+			// Prevent transitions/animations on resize.
+				var resizeTimeout;
+
+				$window.on('resize', function() {
+
+					window.clearTimeout(resizeTimeout);
+
+					$body.addClass('is-resizing');
+
+					resizeTimeout = window.setTimeout(function() {
+						$body.removeClass('is-resizing');
+					}, 100);
+
+				});
+
+		}
+
+	// Scroll back to top.
+		$window.scrollTop(0);
+
+	// Panels.
+		var $panels = $('.panel');
+
+		$panels.each(function() {
+
+			var $this = $(this),
+				$toggles = $('[href="#' + $this.attr('id') + '"]'),
+				$closer = $('<div class="closer" />').appendTo($this);
+
+			// Closer.
+				$closer
+					.on('click', function(event) {
+						$this.trigger('---hide');
+					});
+
+			// Events.
+				$this
+					.on('click', function(event) {
+						event.stopPropagation();
+					})
+					.on('---toggle', function() {
+
+						if ($this.hasClass('active'))
+							$this.triggerHandler('---hide');
+						else
+							$this.triggerHandler('---show');
+
+					})
+					.on('---show', function() {
+
+						// Hide other content.
+							if ($body.hasClass('content-active'))
+								$panels.trigger('---hide');
+
+						// Activate content, toggles.
+							$this.addClass('active');
+							$toggles.addClass('active');
+
+						// Activate body.
+							$body.addClass('content-active');
+
+					})
+					.on('---hide', function() {
+
+						// Deactivate content, toggles.
+							$this.removeClass('active');
+							$toggles.removeClass('active');
+
+						// Deactivate body.
+							$body.removeClass('content-active');
+
+					});
+
+			// Toggles.
+				$toggles
+					.removeAttr('href')
+					.css('cursor', 'pointer')
+					.on('click', function(event) {
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						$this.trigger('---toggle');
+
+					});
+
+		});
+
+		// Global events.
+			$body
+				.on('click', function(event) {
+
+					if ($body.hasClass('content-active')) {
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						$panels.trigger('---hide');
+
+					}
+
+				});
+
+			$window
+				.on('keyup', function(event) {
+
+					if (event.keyCode == 27
+					&&	$body.hasClass('content-active')) {
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						$panels.trigger('---hide');
+
+					}
+
+				});
+
+	// Header.
+		var $header = $('#header');
+
+		// Links.
+			$header.find('a').each(function() {
+
+				var $this = $(this),
+					href = $this.attr('href');
+
+				// Internal link? Skip.
+					if (!href
+					||	href.charAt(0) == '#')
+						return;
+
+				// Redirect on click.
+					$this
+						.removeAttr('href')
+						.css('cursor', 'pointer')
+						.on('click', function(event) {
+
+							event.preventDefault();
+							event.stopPropagation();
+
+							window.location.href = href;
+
+						});
+
+			});
+
+	// Footer.
+		var $footer = $('#footer');
+
+		// Copyright.
+		// This basically just moves the copyright line to the end of the *last* sibling of its current parent
+		// when the "medium" breakpoint activates, and moves it back when it deactivates.
+			$footer.find('.copyright').each(function() {
+
+				var $this = $(this),
+					$parent = $this.parent(),
+					$lastParent = $parent.parent().children().last();
+
+				breakpoints.on('<=medium', function() {
+					$this.appendTo($lastParent);
+				});
+
+				breakpoints.on('>medium', function() {
+					$this.appendTo($parent);
+				});
+
+			});
+
+	// Main.
+		var $main = $('#main');
+
+		// Thumbs.
+			$main.children('.thumb').each(function() {
+
+				var	$this = $(this),
+					$image = $this.find('.image'), $image_img = $image.children('img'),
+					x;
+
+				// No image? Bail.
+					if ($image.length == 0)
+						return;
+
+				// Image.
+				// This sets the background of the "image" <span> to the image pointed to by its child
+				// <img> (which is then hidden). Gives us way more flexibility.
+
+					// Set background.
+						$image.css('background-image', 'url(' + $image_img.attr('src') + ')');
+
+					// Set background position.
+						if (x = $image_img.data('position'))
+							$image.css('background-position', x);
+
+					// Hide original img.
+						$image_img.hide();
+
+			});
+
+		// Poptrox.
+			$('.thumb').each(function() {
+    var $this = $(this);
+    var projectRel = $this.find('a.image').first().attr('rel'); // Pobierz rel z pierwszego obrazka w danym projekcie
+
+    if (projectRel) { // Upewnij się, że rel istnieje
+        // Inicjalizuj Poptrox tylko dla linków z tym konkretnym rel
+        $this.poptrox({ // Użyj $this (czyli danego .thumb) jako kontekstu
+            baseZIndex: 20000,
+            caption: function($a) {
+                var s = '';
+                $a.nextAll().each(function() {
+                    // Sprawdź, czy element jest ukryty i ma ten sam rel
+                    if ($(this).is(':hidden') && $(this).attr('rel') === $a.attr('rel')) {
+                        s += this.outerHTML;
+                    }
+                });
+                return s;
+            },
+            fadeSpeed: 300,
+            onPopupClose: function() { $body.removeClass('modal-active'); },
+            onPopupOpen: function() { $body.addClass('modal-active'); },
+            overlayOpacity: 0,
+            popupCloserText: '',
+            popupHeight: 150,
+            popupLoaderText: '',
+            popupSpeed: 300,
+            popupWidth: 150,
+            // Kluczowa zmiana: Selektor Poptrox musi być ogólny, ale inicjacja jest per grupa.
+            // W tym przypadku poptrox działa na $this (czyli jednym .thumb)
+            // więc selector odnosi się do linków W TYM konkretnym .thumb
+            selector: 'a.image',
+            usePopupCaption: false,
+            usePopupCloser: true,
+            usePopupDefaultStyling: false,
+            usePopupForceClose: true,
+            usePopupLoader: true,
+            usePopupNav: true,
+            windowMargin: 50,
+            loop: false, // To jest ważne, żeby nie zawijało
+            onSlideNextEnd: function() {
+                // Ta funkcja jest teraz w kontekście pojedynczej galerii projektu
+                // więc x.trigger("poptrox_close") powinno działać.
+                // Upewnij się, że używasz prawidłowego odniesienia do instancji Poptrox
+                // W tym kontekście, `this` będzie instancją poptrox
+                // ale żeby użyć triggera na głównym elemencie `x`, potrzebujemy go przekazać
+                // lub zmodyfikować sam plik poptrox.min.js jak w poprzednich krokach.
+                // Najprościej będzie użyć poprzedniej modyfikacji w poptrox.min.js
+                // Wtedy nie potrzebujesz nic tutaj.
+            },
+            onSlidePrevEnd: function() {
+                // Analogicznie, nic tu nie trzeba, jeśli masz modyfikację w poptrox.min.js
+            }
+        });
+    }
+});
+
+			// Hack: Set margins to 0 when 'xsmall' activates.
+				breakpoints.on('<=xsmall', function() {
+					$main[0]._poptrox.windowMargin = 0;
+				});
+
+				breakpoints.on('>xsmall', function() {
+					$main[0]._poptrox.windowMargin = 50;
+				});
+
+})(jQuery);
