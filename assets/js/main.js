0d97ce3cd2d560ba30f72a9fcb310bfadfdd94b4
Marek Sobol <mswizual@gmail.com>
2025-12-31 00:51:59 +0100

    Aktualizacja - Powiekszenie zdjec na telefonach

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,11 +272,11 @@
 });
 
 			// Hack: Set margins to 0 when 'xsmall' activates.
-				breakpoints.on('<=xsmall', function() {
+				breakpoints.on('<=small', function() {
 					$main[0]._poptrox.windowMargin = 0;
 				});
 
-				breakpoints.on('>xsmall', function() {
+				breakpoints.on('>small', function() {
 					$main[0]._poptrox.windowMargin = 50;
 				});
 
